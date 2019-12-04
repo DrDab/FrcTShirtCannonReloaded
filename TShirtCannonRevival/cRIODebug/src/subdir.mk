7fb3c0d8cbdaf452bbc54fd9c9fd12dd95403b54
DrDab <duvictor514@gmail.com>
2019-12-03 21:41:14 -0800

    Added all files

---
+++ TShirtCannonRevival/cRIODebug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/NanoBot.cpp 
+
+OBJS += \
+./src/NanoBot.o 
+
+CPP_DEPS += \
+./src/NanoBot.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Thunder cRIO Tools C++ Compiler'
+	powerpc-wrs-vxworks-g++ -DCPU=PPC603 -DTOOL_FAMILY=gnu -DTOOL=gnu -D_WRS_KERNEL -I"C:\Users\YukiLeopard\eclipse-workspace\TShirtCannonRevival\include" -I"C:\Program Files (x86)\FRC_Toolchain\mingw\powerpc-wrs-vxworks\wind_base\/../include/WPILib" -O0 -g3 -Wall -c -fmessage-length=0 -mcpu=603 -mstrict-align -mlongcall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
